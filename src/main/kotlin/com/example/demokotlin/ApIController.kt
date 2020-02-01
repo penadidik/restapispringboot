0cb449231a55ae91e82ec75171de14a20e910ec8
penadidik <di2k.skh@gmail.com>
2020-02-02 00:49:17 +0700

    create api

---
+++ src/main/kotlin/com/example/demokotlin/ApIController.kt
@@ -0,0 +1,19 @@
+package com.example.demokotlin
+
+import com.example.demokotlin.service.DemoService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class ApIController {
+
+    @Autowired
+    private lateinit var demoService: DemoService
+
+    @GetMapping("/demo/alldata")
+    fun getAllData(): List<DemoModel> = demoService.findAllDemo()
+
+}
